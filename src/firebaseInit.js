c5976cda7c499783c07b89bdc0247edbfd9a6db3
helen-cho <58962560+helen-cho@users.noreply.github.com>
2023-05-16 16:29:43 +0900

    Update firebaseInit.js

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -7,14 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_vW4PgubNFxm1kEwn291_zAMmK88mDMw",
-  authDomain: "fir-7ea02.firebaseapp.com",
-  databaseURL: "https://fir-7ea02-default-rtdb.firebaseio.com",
-  projectId: "fir-7ea02",
-  storageBucket: "fir-7ea02.appspot.com",
-  messagingSenderId: "910739164003",
-  appId: "1:910739164003:web:c8b6ffcb62cee5ac926156",
-  measurementId: "G-PRHDH233L8"
+
 };
 
 // Initialize Firebase
